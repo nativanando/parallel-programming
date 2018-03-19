14d5869cd89bea6b23f0e1c8a54d2033813482e6
natividade <fernando.dnl@bolsista.pti.org.br>
2018-03-19 13:54:07 -0300

    Redirecting folders and adding python support

---
+++ samples/basic/samples-python/mpi4pySharing.py
@@ -0,0 +1,21 @@
+"""
+This file is a test for shared mpi4py libraries in a cluster environment.
+The application can be run by: mpiexec --hostfile /tmp/machines -np $N_PROCES --allow-run-as-root python mpi4pySharing.py
+"""
+__author__ = 'Fernando Demarchi Natividade Luiz'
+__email__ = "nativanando@gmail.com"
+
+import sys
+
+#Redirecting the library of mpi4py to network file system path shared
+sys.path.append('/opt/ohpc/pub/libs/gnu7/openmpi3/mpi4py/2.0.0/lib64/python2.7/site-packages/mpi4py/')
+import MPI
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+
+if rank == 0:
+    data = {'a': 7, 'b': 3.14}
+    comm.send(data, dest=1, tag=11)
+elif rank == 1:
+    data = comm.recv(source=0, tag=11)
