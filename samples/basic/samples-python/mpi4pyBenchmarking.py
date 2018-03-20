e70c2acf29ee3c0d370c16e5f28234b0b409901d
natividade <fernando.dnl@bolsista.pti.org.br>
2018-03-20 17:03:03 -0300

    updating benchmark configuration with mpi4py

--- samples/basic/samples-python/mpi4pyBenchmarking.py
+++ samples/basic/samples-python/mpi4pyBenchmarking.py
@@ -11,14 +11,16 @@ import sys
 from time import time
 import numpy as np
 
-#Redirecting the library of mpi4py to network file system path shared
+# Redirecting the library of mpi4py to network file system path shared
 sys.path.append('/opt/ohpc/pub/libs/gnu7/openmpi3/mpi4py/2.0.0/lib64/python2.7/site-packages/mpi4py/')
 import MPI
 
-sizes = [ 2**n for n in xrange(1,24) ] #number of bytes
-runs  = 20 #number of iterations
+sizes = [ 2**n for n in xrange(1,24) ] # size data [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608]
+runs  = 20 # number of iterations
 
 comm = MPI.COMM_WORLD
+rank = comm.Get_rank() #number of running processor
+
 
 print("Benchmarking braodcast performance on %d parallel MPI processes..." % comm.size)
 print()
@@ -27,13 +29,10 @@ print("%15s | %12s | %12s" %
 
 for s in sizes:
     data = np.ones(s) # ([ s.,  s.,  s.,  s.,  s.]) # return a new array of given shape and type.
-
-    comm.Barrier()
+    comm.Barrier() # Global synchronisation operation
     t0 = time()
     for i in xrange(runs):
-        comm.Bcast( [data, MPI.DOUBLE], 0)
-    comm.Barrier()
+        comm.Bcast( [data, MPI.DOUBLE], 0) # Broadcast a message from one process to all other processes in a group
+    comm.Barrier() # Global synchronisation operation
     t = (time()-t0) / runs
-
-    print("%15d | %12.3f | %12.3f" %
-        (data.nbytes, t*1000, data.nbytes/t/1024/1024) )
+    print("%15d | %12.3f | %12.3f" % (data.nbytes, t*1000, data.nbytes/t/1024/1024) ) #number bytes, time (msec) and bandwidth (mybytes)
