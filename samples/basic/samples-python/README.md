846572a27745171fb702f43f34f97202e349e208
natividade <fernando.dnl@bolsista.pti.org.br>
2018-03-21 10:26:25 -0300

    updating file of barrier synchronization

---
+++ samples/basic/samples-python/README.md
@@ -0,0 +1,14 @@
+# Intro to mpi4py library
+
+There are many situations in parallel programming when groups of processes need to exchange messages. Rather than explicitly sending and receiving such messages as we have been doing, the real power of MPI comes from group operations known as collectives.
+
+### [MPI for python documentation](http://mpi4py.scipy.org/docs/usrman/index.html)
+
+## Colletctive operations
+
+Synchronization operation. Creates a barrier synchronization in a group. Each task, when reaching the Barrier() call, blocks until all tasks in the group reach a Barrier() call. Then all tasks are free to proceed.
+
+```Bash
+# Add additional directories to the path.
+comm.Barrier()
+```
--- samples/basic/samples-python/barrier_synchronisation.py
+++ samples/basic/samples-python/barrier_synchronisation.py
@@ -4,7 +4,7 @@
 This file is a test for shared mpi4py libraries in a cluster environment.
 The application can be run by: mpiexec --hostfile /tmp/machines -np $N_PROCES --allow-run-as-root python mpi4pySharing.py
 """
-__author__ = 'Fernando Demarchi Natividade Luiz'
+__author__ = "Fernando Demarchi Natividade Luiz"
 __email__ = "nativanando@gmail.com"
 
 import sys
@@ -12,7 +12,7 @@ from time import time
 import numpy as np
 
 # Redirecting the library of mpi4py to network file system path shared
-sys.path.append('/opt/ohpc/pub/libs/gnu7/openmpi3/mpi4py/2.0.0/lib64/python2.7/site-packages/mpi4py/')
+sys.path.append("/opt/ohpc/pub/libs/gnu7/openmpi3/mpi4py/2.0.0/lib64/python2.7/site-packages/mpi4py/")
 import MPI
 
 sizes = [ 2**n for n in xrange(1,24) ] # size data [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608]
@@ -29,7 +29,6 @@ print("%15s | %12s | %12s" %
 
 for s in sizes:
     data = np.ones(s) # ([ s.,  s.,  s.,  s.,  s.]) # return a new array of given shape and type.
-    comm.Barrier() # Global synchronisation operation
     t0 = time()
     for i in xrange(runs):
         comm.Bcast( [data, MPI.DOUBLE], 0) # Broadcast a message from one process to all other processes in a group
