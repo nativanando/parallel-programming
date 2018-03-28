9ff524ab8e0e55d1c219ae4e18b8fa62461d9513
nativanando <nativanando@gmail.com>
2018-03-28 15:45:58 -0300

    updating the split of data to reduce method

--- samples/basic/samples-python/model/scatter_synchronisation.py
+++ samples/basic/samples-python/model/scatter_synchronisation.py
@@ -9,12 +9,14 @@ import sys
 # Redirecting the library of mpi4py to network file system path shared
 sys.path.append("/opt/ohpc/pub/libs/gnu7/openmpi3/mpi4py/2.0.0/lib64/python2.7/site-packages/mpi4py/")
 import MPI
+from time import time
 
 class ScatterSynchronisation(object):
     def __init__(self):
         self.data = None
         self.data_gather = None
         self.chunks = None
+        self.reduction_data = None
         self.comm = MPI.COMM_WORLD
         self.size = self.comm.Get_size()
         self.rank = self.comm.Get_rank()
@@ -27,14 +29,23 @@ class ScatterSynchronisation(object):
             for i, chunk in enumerate(self.data):
                 self.chunks[i % self.size].append(chunk) # Increasing in the array, starting from the initial one, and dividing it according to the amount of processors
 
-    def run_scatter(self):
+    def run_scatter_reduce(self):
+        t0 = time()
         self.data = self.comm.scatter(self.chunks, root=0) # Defines the data and the process that contains the data to be distributed
         print('rank' + str(self.rank) + ' has data:' + str(self.data)) # Print the datas available in each processes distributed by initial Processor
         self.data_gather = self.comm.gather(self.data, root=0) # Gather the data for a specific processor
+        for i in range(len(self.data)): # Performs the calculation of the split data
+            self.data[i] = self.data[i] + 1
+        self.reduction_data = self.comm.reduce(self.data, op=MPI.SUM, root=0) #reduce data to new array with calculation
+        self.comm.Barrier()
+        t = (time()-t0) * 1000
+        print(t)
+
         if self.rank == 0:
             print(self.data_gather)
+            print(self.reduction_data)
 
 if __name__ == '__main__':
     scatter = ScatterSynchronisation()
     scatter.create_array_to_scattering()
-    scatter.run_scatter()
+    scatter.run_scatter_reduce()
