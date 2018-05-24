ca7e6b6b93f82b6d43e0c81f90aec7f8cf4645b0
fernando.luiz <fernando.luiz@pti.org.br>
2018-05-24 15:28:41 -0300

    Ploting benchmark

--- samples/foam/cavityParallel/plot.py
+++ samples/foam/cavityParallel/plot.py
@@ -10,19 +10,27 @@ import csv
 import pandas as pd
 
 class BenchMarkSpeedUp:
-	def __init__(self, file_in):
-		self.file_in = file_in
-		self.df = pd.read_csv('results/'+self.file_in)
-		print (self.df['time'])
-	
+	def __init__(self):
+		self.df1 = pd.read_csv('results/log-out-2x1.txt')
+		self.df2 = pd.read_csv('results/log-out-2x2.txt')
+		self.df3 = pd.read_csv('results/log-out-4x2.txt')
+		self.df4 = pd.read_csv('results/log-out-6x2.txt')
+		self.df5 = pd.read_csv('results/log-out-4x4.txt')
+
 	def plot_graph(self):
-		plt.plot(self.df['time'])
-		plt.title('Cluster broadcasting')
+		# plt.plot(self.df1['time'])
+		plt.plot(self.df1['time'], label="2 processors")
+		plt.plot(self.df2['time'], label="4 processors")
+		plt.plot(self.df3['time'], label="8 processors")
+		plt.plot(self.df4['time'], label="12 processors")
+		plt.plot(self.df5['time'], label="16 processors")
+		plt.title('Processors Time in openFOAM Simulation')
+		plt.legend(loc='upper left')
+		plt.xlabel("cycles")
+		plt.ylabel("Time (sec)")
    		plt.grid(True)
-        	plt.savefig('plot.png')
+        	plt.savefig('plot2.png')
 
 if __name__ == '__main__':
-	plot = BenchMarkSpeedUp('log-out-2x1.txt')
+	plot = BenchMarkSpeedUp()
 	plot.plot_graph()
-
-
