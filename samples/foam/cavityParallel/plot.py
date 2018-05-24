dc4cb9ea3324f7fc8f0237ffc3c3001e073c7c7d
fernando.luiz <fernando.luiz@pti.org.br>
2018-05-24 17:32:27 -0300

    Graph okgit status =)

--- samples/foam/cavityParallel/plot.py
+++ samples/foam/cavityParallel/plot.py
@@ -11,25 +11,27 @@ import pandas as pd
 
 class BenchMarkSpeedUp:
 	def __init__(self):
-		self.df1 = pd.read_csv('results/log-out-2x1.txt')
-		self.df2 = pd.read_csv('results/log-out-2x2.txt')
-		self.df3 = pd.read_csv('results/log-out-4x2.txt')
-		self.df4 = pd.read_csv('results/log-out-6x2.txt')
-		self.df5 = pd.read_csv('results/log-out-4x4.txt')
+		self.df1 = pd.read_csv('results/log-out-1x1.txt')
+		self.df2 = pd.read_csv('results/log-out-2x1.txt')
+		self.df3 = pd.read_csv('results/log-out-2x2.txt')
+		self.df4 = pd.read_csv('results/log-out-4x2.txt')
+		self.df5 = pd.read_csv('results/log-out-6x2.txt')
+		self.df6 = pd.read_csv('results/log-out-4x4.txt')
 
 	def plot_graph(self):
 		# plt.plot(self.df1['time'])
-		plt.plot(self.df1['time'], label="2 processors")
-		plt.plot(self.df2['time'], label="4 processors")
-		plt.plot(self.df3['time'], label="8 processors")
-		plt.plot(self.df4['time'], label="12 processors")
-		plt.plot(self.df5['time'], label="16 processors")
-		plt.title('CELTAB cluster metric')
+		plt.plot(self.df1['time'], label="1 processor")
+		plt.plot(self.df2['time'], label="2 processors")
+		plt.plot(self.df3['time'], label="4 processors")
+		plt.plot(self.df4['time'], label="8 processors")
+		plt.plot(self.df5['time'], label="12 processors")
+		plt.plot(self.df6['time'], label="16 processors")
+		plt.title('CELTAB Cluster Metrics')
 		plt.legend(loc='upper left')
 		plt.xlabel("Epochs")
 		plt.ylabel("Execution time (sec)")
    		plt.grid(True)
-        	plt.savefig('plot2.png')
+        	plt.savefig('benchmark.png')
 
 if __name__ == '__main__':
 	plot = BenchMarkSpeedUp()
