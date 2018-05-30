b9dd5f49d4d55c013060e7749a3be8c9336d0551
nativanando <nativanando@gmail.com>
2018-05-30 16:02:34 -0300

    adding benchmarking class D

--- samples/NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/jobslurm.sh
+++ samples/NPB3.3.1-MZ/NPB3.3-MZ-MPI/bin/jobslurm.sh
@@ -5,11 +5,10 @@
 
 #SBATCH --time=20:00:00   # walltime limit (HH:MM:SS)
 #SBATCH --nodes=4   # number of nodes
-#SBATCH --ntasks-per-node=4   # 4 processor core(s) per node
+#SBATCH --ntasks-per-node=8   # 4 processor core(s) per node
 #SBATCH --job-name="nativa-jobs"
 #SBATCH --output="slurm-%j.out.log" # job standard output file (%j replaced by job id)
 #SBATCH --error="slurm-%j.err.out.log" # job standard error file (%j replaced by job id)
-
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 
-mpiexec --hostfile machines --allow-run-as-root bt-mz.B.2
+mpiexec --hostfile machines --allow-run-as-root bt-mz.D.32
