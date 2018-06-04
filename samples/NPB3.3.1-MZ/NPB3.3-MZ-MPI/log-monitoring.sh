4b52d9078c42c527a3064b816d49aa9b315bab56
nativanando <nativanando@gmail.com>
2018-06-04 10:04:14 -0300

    adding ansys mesh

--- samples/NPB3.3.1-MZ/NPB3.3-MZ-MPI/log-monitoring.sh
+++ samples/NPB3.3.1-MZ/NPB3.3-MZ-MPI/log-monitoring.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-cat ./bin/out-* | grep seconds | sed 's/[a-z A-Z =]\+//g' | sort -rn
-
+cat ./bin/out-* | grep seconds | sed 's/[a-z A-Z =]\+//g' | sort -rn  > csv/execution-time.txt && sed -i "1s/^/execution-time\n/" csv/execution-time.txt  && \
+cat ./bin/out-* | grep Total | grep process | sed 's/[a-z A-Z =]\+//g' | sort -n > csv/amount-np.txt && sed -i "1s/^/processors\n/" csv/amount-np.txt && \
+cat ./bin/out-* | grep Mop/s | grep total | sed 's/[a-z A-Z = /]\+//g' | sort -n > csv/total-mops.txt && sed -i "1s/^/total-mops\n/" csv/total-mops.txt && \
+cat ./bin/out-* | grep Mop/s | grep thread | sed 's/[a-z A-Z = /]\+//g' | sort -nr > csv/amount-thread-mops.txt && sed -i "1s/^/thread-mops\n/" csv/amount-thread-mops.txt
